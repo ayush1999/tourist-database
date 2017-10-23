93da5f6888fe194c6d367fac9a838e25ab1690d1
ayush1999 <ayush.shridhar1999@gmail.com>
2017-10-23 19:46:38 +0530

    lodging.h added

---
+++ lodging.h
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+class lodging{
+    string s;
+public:
+    void show(){
+                            //  Name of all place      Lodging facility
+    }
+    void search(){
+                            // Name of place searched for   Lodging facility
+    }
+};
+
+ 
