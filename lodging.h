e40e6e17f07b3c439ad3ab40d211cc68e7f0b0bb
ayush1999 <ayush.shridhar1999@gmail.com>
2017-10-23 20:03:06 +0530

    lodging.h Updated

--- lodging.h
+++ lodging.h
@@ -4,13 +4,21 @@
 using namespace std;
 
 class lodging{
-    string s;
+    int choice;
 public:
-    void show(){
-                            //  Name of all place      Lodging facility
+    lodging(){
+        cout<<"Enter the type of place : 1. Beaches"<<endl;
+        cout<<"                          2. Amusement Parks"<<endl;
+        cout << "                        3. Zoo" << endl;
+        cout << "                        4. Temples" << endl;
+        cout << "                        5. Hill Station" << endl;
+        cout << "                        6. Dams" << endl;
+        cout << "                        7. Gardens" << endl;
+        cin >> choice;
     }
-    void search(){
-                            // Name of place searched for   Lodging facility
+
+    void show(){
+                            //  Using switch case, open the corresponding csv file.
     }
 };
 
