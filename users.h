9a0d4a4b8726be31553e870bd8b45982abe88004
ayush1999 <ayush.shridhar1999@gmail.com>
2017-10-23 19:18:51 +0530

    BaseUser added

---
+++ users.h
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+class BaseUser{
+    int choice;
+public:
+    BaseUser(){
+        cout<<"Are you a 1. User"<<endl;
+        cout<<"          2. Managing Staff";
+        cin>>choice;
+
+    }
+    void view(){
+        ofstream in;
+        in.open("name_of_csv.csv");
+        
+    }
+    int get_type(){
+        return choice;
+    };
+};
+
+class User: public BaseUser{
+
+};
+
+class managing_staff: public BaseUser{
+public:
+    void edit(){
+        int option;
+        cout<<"What Do you want 1. Add"<<endl;
+        cout<<"                 2. Edit";
+        cin>>option;
+        if (option==1){
+                                        // Append to the csv file
+        }
+        else{
+                                        // Edit the csv file
+        }
+    }
+}
+
