567505903a0f0c98d50c0ad18183d41756d15e9c
ayush1999 <ayush.shridhar1999@gmail.com>
2017-10-23 20:09:48 +0530

    remaining added

---
+++ transport.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class transport
+{
+    int choice;
+
+  public:
+    transport()
+    {
+        cout << "Enter the type of place : 1. Beaches" << endl;
+        cout << "                          2. Amusement Parks" << endl;
+        cout << "                          3. Zoo" << endl;
+        cout << "                          4. Temples" << endl;
+        cout << "                          5. Hill Station" << endl;
+        cout << "                          6. Dams" << endl;
+        cout << "                          7. Gardens" << endl;
+        cin >> choice;
+    }
+    void show()
+    {
+        // using switch case, read corresponding csv.(only transport column)
+    }
+}
