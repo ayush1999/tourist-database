122eb54f70bb11d26fbcdf3e4ab0829b89adff28
Abhipsha Das <abhipsha.das16@gmail.com>
2017-10-24 21:06:51 +0530

    Update transport.h

--- transport.h
+++ transport.h
@@ -12,11 +12,10 @@ class transport
     {
         cout << "Enter the type of place : 1. Beaches" << endl;
         cout << "                          2. Amusement Parks" << endl;
-        cout << "                          3. Zoo" << endl;
+        cout << "                          3. Zoos and Wildlife Sanctuaries" << endl;
         cout << "                          4. Temples" << endl;
         cout << "                          5. Hill Station" << endl;
-        cout << "                          6. Dams" << endl;
-        cout << "                          7. Gardens" << endl;
+        cout << "                          6. Dams and Lakes" << endl;
         cin >> choice;
     }
     void show()
