3985e13f34779ef25bf7333c2c0b436a990b8571
Abhipsha Das <abhipsha.das16@gmail.com>
2017-10-25 14:15:26 +0530

    Update transport.h
    The string _**s**_ must contain the entire row entry.
    I have yet to figure out how to do that.
    See if this code works for reading two columns. Then we can apply it to lodging, places, etc. as well.

--- transport.h
+++ transport.h
@@ -21,5 +21,34 @@ class transport
     void show()
     {
         // using switch case, read corresponding csv.(only transport column)
+        switch(choice)
+        {
+        	case 1: ifstream file ;
+                	file.open("beach.csv");
+        	        string delimiter=",";
+        	        string s="";//s stores the data from an entire row to a string
+        	        //store in s then continue
+        	        
+        	        cout<<"Available transportation modes:"<<endl;
+        	        
+        	        size_t pos = 0;
+					string token;
+					int counter = 0;
+					while ((pos = s.find(delimiter)) != std::string::npos) 
+					{
+					    token = s.substr(0, pos);
+
+                         if(counter == 0 || counter == 2)
+                         {
+                             cout << token << endl;
+                         }
+
+                         s.erase(0, pos + delimiter.length());
+                    }
+                    file.close();
+					break;
+			
+		           //Other cases similar to case 1 with corresponding file invocation
+		}//end of switch()
     }
 }
