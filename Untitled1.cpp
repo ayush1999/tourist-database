5c4f567b479eaf2ec21276a6537bc7eeef01b1fb
ashrit001 <32726109+ashrit001@users.noreply.github.com>
2017-10-29 22:28:34 +0530

    Add files via upload
    this is just for reading file from beaches.csv

---
+++ Untitled1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+using namespace std;
+char* getl(ifstream f,char a)
+{
+    char *n,c;
+    int i=0;
+    while(!f.eof())
+    {
+        f>>c;
+        while(c!=a)
+        {
+            *(n+i)=c;
+            i++;
+        }
+
+    }
+    return n;
+}
+int main()
+ {
+  fstream f;
+  f.open("sample.csv",ios::out);
+  f<<"1,2,3,4,5";
+  f.close();
+  ifstream fin;
+  fin.open("beach.csv");
+  char *n=new char[100];
+  char *a=new char[500];
+  char *tr=new char[100];
+  char *ti=new char[100];
+  while(fin)
+  {
+  fin.getline(n,100,',');
+  fin.getline(a,500,',');
+  fin.getline(tr,100,',');
+  fin.getline(ti,100);
+  cout<<n<<"\t\t"<<ti<<endl;
+  }
+  return 0;
+}
